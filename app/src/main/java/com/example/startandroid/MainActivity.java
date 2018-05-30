366a00b10115ebb92852a17745c3e36be3bec5d2
Jon <djonli@mail.ua>
2018-05-30 14:42:16 +0300

    - lesson 15

--- app/src/main/java/com/example/startandroid/MainActivity.java
+++ app/src/main/java/com/example/startandroid/MainActivity.java
@@ -1,10 +1,11 @@
 package com.example.startandroid;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
+import android.view.ContextMenu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -16,41 +17,79 @@ import android.widget.Toast;
 import org.w3c.dom.Text;
 
 public class MainActivity extends Activity  {
-    TextView tv;
-    CheckBox chb;
+    private static final byte MENU_COLOR_BLUE = 3;
+    private static final byte MENU_COLOR_RED = 1;
+    private static final byte MENU_COLOR_GREEN = 2;
+    private static final byte MENU_SIZE_22 = 4;
+    private static final byte MENU_SIZE_26 = 5;
+    private static final byte MENU_SIZE_30 = 6;
+
+
+    TextView tvColor, tvSize;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-       tv = findViewById(R.id.textView);
-       chb = findViewById(R.id.chbExtMenu);
-        }
-        public boolean onCreateOptionsMenu(Menu menu) {
-            menu.add(0, 1, 0, "add");
-            menu.add(0, 2, 0, "edit");
-            menu.add(0, 3, 3, "delete");
-            menu.add(1, 4, 1, "copy");
-            menu.add(1, 5, 2, "paste");
-            menu.add(1, 6, 4, "exit");
+        tvColor = findViewById(R.id.tvColor);
+        tvSize = findViewById(R.id.tvSize);
 
-            return super.onCreateOptionsMenu(menu);
-        }
-        public boolean onPrepareOptionsMenu(Menu menu){
-        menu.setGroupVisible(1,chb.isChecked());
-        return super.onPrepareOptionsMenu(menu);
-        }
-        public  boolean onOptionsItemSelected(MenuItem item){
-            StringBuilder sb = new StringBuilder();
-            sb.append("Item Menu");
-            sb.append("\r\n groupId: " + String.valueOf(item.getGroupId()));
-            sb.append("\r\n itemId: " + String.valueOf(item.getItemId()));
-            sb.append("\r\n order: " + String.valueOf(item.getOrder()));
-            sb.append("\r\n title: " + item.getTitle());
-            tv.setText(sb.toString());
-
-            return super.onOptionsItemSelected(item);
+        registerForContextMenu(tvColor);
+        registerForContextMenu(tvSize);
+    }
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        // TODO Auto-generated method stub
+        switch (v.getId()) {
+            case R.id.tvColor:
+                menu.add(0, MENU_COLOR_RED, 0, "Red");
+                menu.add(0, MENU_COLOR_GREEN, 0, "Green");
+                menu.add(0, MENU_COLOR_BLUE, 0, "Blue");
+                break;
+            case R.id.tvSize:
+                menu.add(0, MENU_SIZE_22, 0, "22");
+                menu.add(0, MENU_SIZE_26, 0, "26");
+                menu.add(0, MENU_SIZE_30, 0, "30");
+                break;
         }
     }
+    public boolean onContextItemSelected(MenuItem item) {
+        // TODO Auto-generated method stub
+        switch (item.getItemId()) {
+            // пункты меню для tvColor
+            case MENU_COLOR_RED:
+                tvColor.setTextColor(Color.RED);
+                tvColor.setText("Text color = red");
+                break;
+            case MENU_COLOR_GREEN:
+                tvColor.setTextColor(Color.GREEN);
+                tvColor.setText("Text color = green");
+                break;
+            case MENU_COLOR_BLUE:
+                tvColor.setTextColor(Color.BLUE);
+                tvColor.setText("Text color = blue");
+                break;
+// пункты меню для tvSize
+            case MENU_SIZE_22:
+                tvSize.setTextSize(22);
+                tvSize.setText("Text size = 22");
+                break;
+            case MENU_SIZE_26:
+                tvSize.setTextSize(26);
+                tvSize.setText("Text size = 26");
+                break;
+            case MENU_SIZE_30:
+                tvSize.setTextSize(30);
+                tvSize.setText("Text size = 30");
+                break;
+        }
+        return super.onContextItemSelected(item);
+    }
+    public void registerForContextMenu(View view) {
+        view.setOnCreateContextMenuListener(this);
+    }
+}
 
 
 
