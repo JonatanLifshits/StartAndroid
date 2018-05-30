ecd7f53a0326afebc179deb63779873729fedef4
Jon <djonli@mail.ua>
2018-05-30 14:31:49 +0300

    - lesson 14

--- app/src/main/java/com/example/startandroid/MainActivity.java
+++ app/src/main/java/com/example/startandroid/MainActivity.java
@@ -15,34 +15,43 @@ import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
-public class MainActivity extends Activity {
-    /** Called when the activity is first created. */
+public class MainActivity extends Activity  {
+    TextView tv;
+    CheckBox chb;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+       tv = findViewById(R.id.textView);
+       chb = findViewById(R.id.chbExtMenu);
+        }
+        public boolean onCreateOptionsMenu(Menu menu) {
+            menu.add(0, 1, 0, "add");
+            menu.add(0, 2, 0, "edit");
+            menu.add(0, 3, 3, "delete");
+            menu.add(1, 4, 1, "copy");
+            menu.add(1, 5, 2, "paste");
+            menu.add(1, 6, 4, "exit");
+
+            return super.onCreateOptionsMenu(menu);
+        }
+        public boolean onPrepareOptionsMenu(Menu menu){
+        menu.setGroupVisible(1,chb.isChecked());
+        return super.onPrepareOptionsMenu(menu);
+        }
+        public  boolean onOptionsItemSelected(MenuItem item){
+            StringBuilder sb = new StringBuilder();
+            sb.append("Item Menu");
+            sb.append("\r\n groupId: " + String.valueOf(item.getGroupId()));
+            sb.append("\r\n itemId: " + String.valueOf(item.getItemId()));
+            sb.append("\r\n order: " + String.valueOf(item.getOrder()));
+            sb.append("\r\n title: " + item.getTitle());
+            tv.setText(sb.toString());
+
+            return super.onOptionsItemSelected(item);
+        }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // TODO Auto-generated method stub
-
-        menu.add("menu1");
-        menu.add("menu2");
-        menu.add("menu3");
-        menu.add("menu4");
-
-        return super.onCreateOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // TODO Auto-generated method stub
-        Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
-        return super.onOptionsItemSelected(item);
-    }
-
-}
 
 
 
