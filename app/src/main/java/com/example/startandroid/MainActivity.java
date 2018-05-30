e2379f436c5f4328c2af43234c53c53657b702e8
Jon <djonli@mail.ua>
2018-05-30 14:12:26 +0300

    - lesson 11

--- app/src/main/java/com/example/startandroid/MainActivity.java
+++ app/src/main/java/com/example/startandroid/MainActivity.java
@@ -6,47 +6,26 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
-public class MainActivity extends Activity implements View.OnClickListener {
-
-    TextView tvOut;
-    Button btnOk;
-    Button btnCancel;
-
-    /** Called when the activity is first created. */
-    @Override
+public class MainActivity extends Activity  {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        // найдем View-элементы
-        tvOut = (TextView) findViewById(R.id.tvOut);
-        btnOk = (Button) findViewById(R.id.btnOk);
-        btnCancel = (Button) findViewById(R.id.btnCancel);
+        LinearLayout llBottom = (LinearLayout) findViewById(R.id.llBottom);
+        TextView tvBottom = (TextView) findViewById(R.id.tvBottom);
+        Button btnBottom = (Button) findViewById(R.id.btnBottom);
 
-        // присваиваем обработчик кнопкам
-        btnOk.setOnClickListener(this);
-        btnCancel.setOnClickListener(this);
+        llBottom.setBackgroundResource(R.color.llBottomColor);
+        tvBottom.setText(R.string.tvBottomText);
+        btnBottom.setText(R.string.btnBottomText);
+
     }
-
-    @Override
-    public void onClick(View v) {
-        // по id определеяем кнопку, вызвавшую этот обработчик
-        switch (v.getId()) {
-            case R.id.btnOk:
-                // кнопка ОК
-                tvOut.setText("Нажата кнопка ОК");
-                break;
-            case R.id.btnCancel:
-                // кнопка Cancel
-                tvOut.setText("Нажата кнопка Cancel");
-                break;
-        }
-    }
-
+}
 }
 
 
