adcec9df54e72a308e82085090852618cb652776
Jon <djonli@mail.ua>
2018-05-30 14:20:06 +0300

    - lesson 13

--- app/src/main/java/com/example/startandroid/MainActivity.java
+++ app/src/main/java/com/example/startandroid/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -13,50 +15,34 @@ import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
-public class MainActivity extends Activity  {
-    TextView tvOut;
-    Button btnOk;
-    Button btnCancel;
-
-    private static final String TAG = "myLogs";
-
+public class MainActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
-        // найдем View-элементы
-        Log.d(TAG, "найдем View-элементы");
-        tvOut = (TextView) findViewById(R.id.tvOut);
-        btnOk = (Button) findViewById(R.id.btnOk);
-        btnCancel = (Button) findViewById(R.id.btnCancel);
-
-        // присваиваем обработчик кнопкам
-        Log.d(TAG, "присваиваем обработчик кнопкам");
-        btnOk.setOnClickListener(this);
-        btnCancel.setOnClickListener(this);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // TODO Auto-generated method stub
 
-    public void onClick(View v) {
-        // по id определяем кнопку, вызвавшую этот обработчик
-        Log.d(TAG, "по id определяем кнопку, вызвавшую этот обработчик");
-        switch (v.getId()) {
-            case R.id.btnOk:
-                // кнопка ОК
-                Log.d(TAG, "кнопка ОК");
-                tvOut.setText("Нажата кнопка ОК");
-                Toast.makeText(this, "Нажата кнопка ОК", Toast.LENGTH_LONG).show();
-                break;
-            case R.id.btnCancel:
-                // кнопка Cancel
-                Log.d(TAG, "кнопка Cancel");
-                tvOut.setText("Нажата кнопка Cancel");
-                Toast.makeText(this, "Нажата кнопка Cancel", Toast.LENGTH_LONG).show();
-                break;
-        }
+        menu.add("menu1");
+        menu.add("menu2");
+        menu.add("menu3");
+        menu.add("menu4");
+
+        return super.onCreateOptionsMenu(menu);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // TODO Auto-generated method stub
+        Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
+    }
+
+}
 
 
 
