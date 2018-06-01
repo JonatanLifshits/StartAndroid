0fd363064df53aa84f678e8bc5f72d385b21428f
Jon <djonli@mail.ua>
2018-06-01 21:14:30 +0300

    - lesson 61

--- app/src/main/java/com/example/startandroid/MainActivity.java
+++ app/src/main/java/com/example/startandroid/MainActivity.java
@@ -1,71 +1,48 @@
 package com.example.startandroid;
 
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    final int DIALOG_EXIT = 1;
+    final static String LOG_TAG = "myLogs";
+    final int DIALOG = 1;
+    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
 
     /** Called when the activity is first created. */
+    @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
 
     public void onclick(View v) {
-        // вызываем диалог
-        showDialog(DIALOG_EXIT);
+        showDialog(DIALOG);
     }
 
     protected Dialog onCreateDialog(int id) {
-        if (id == DIALOG_EXIT) {
+        Log.d(LOG_TAG, "onCreateDialog");
+        if (id == DIALOG) {
             AlertDialog.Builder adb = new AlertDialog.Builder(this);
-            // заголовок
-            adb.setTitle(R.string.exit);
-            // сообщение
-            adb.setMessage(R.string.save_data);
-            // иконка
-            adb.setIcon(android.R.drawable.ic_dialog_info);
-            // кнопка положительного ответа
-            adb.setPositiveButton(R.string.yes, myClickListener);
-            // кнопка отрицательного ответа
-            adb.setNegativeButton(R.string.no, myClickListener);
-            // кнопка нейтрального ответа
-            adb.setNeutralButton(R.string.cancel, myClickListener);
-            // создаем диалог
+            adb.setTitle("Текущее время");
+            adb.setMessage(sdf.format(new Date(System.currentTimeMillis())));
             return adb.create();
         }
         return super.onCreateDialog(id);
     }
 
-    OnClickListener myClickListener = new OnClickListener() {
-        public void onClick(DialogInterface dialog, int which) {
-            switch (which) {
-                // положительная кнопка
-                case Dialog.BUTTON_POSITIVE:
-                    saveData();
-                    finish();
-                    break;
-                // негативная кнопка
-                case Dialog.BUTTON_NEGATIVE:
-                    finish();
-                    break;
-                // нейтральная кнопка
-                case Dialog.BUTTON_NEUTRAL:
-                    break;
-            }
+    protected void onPrepareDialog(int id, Dialog dialog) {
+        super.onPrepareDialog(id, dialog);
+        Log.d(LOG_TAG, "onPrepareDialog");
+        if (id == DIALOG) {
+            ((AlertDialog)dialog).setMessage(sdf.format(new Date(System.currentTimeMillis())));
         }
-    };
-
-    void saveData() {
-        Toast.makeText(this, R.string.saved, Toast.LENGTH_SHORT).show();
     }
 }
-
