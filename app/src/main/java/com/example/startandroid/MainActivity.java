6ddf44af57f45a91a3219e21d34d9a820e3a8cfc
Jon <djonli@mail.ua>
2018-06-01 16:37:11 +0300

    - lesson 16

--- app/src/main/java/com/example/startandroid/MainActivity.java
+++ app/src/main/java/com/example/startandroid/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.ContextMenu;
+import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -13,83 +14,53 @@ import android.widget.CheckBox;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import org.w3c.dom.Text;
 
-public class MainActivity extends Activity  {
-    private static final byte MENU_COLOR_BLUE = 3;
-    private static final byte MENU_COLOR_RED = 1;
-    private static final byte MENU_COLOR_GREEN = 2;
-    private static final byte MENU_SIZE_22 = 4;
-    private static final byte MENU_SIZE_26 = 5;
-    private static final byte MENU_SIZE_30 = 6;
-
-
-    TextView tvColor, tvSize;
-
+public class MainActivity extends Activity {
+    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        tvColor = findViewById(R.id.tvColor);
-        tvSize = findViewById(R.id.tvSize);
+        // создание LinearLayout
+        LinearLayout linLayout = new LinearLayout(this);
+        // установим вертикальную ориентацию
+        linLayout.setOrientation(LinearLayout.VERTICAL);
+        // создаем LayoutParams
+        LinearLayout.LayoutParams linLayoutParam = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        // устанавливаем linLayout как корневой элемент экрана
+        setContentView(linLayout, linLayoutParam);
 
-        registerForContextMenu(tvColor);
-        registerForContextMenu(tvSize);
-    }
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v,
-                                    ContextMenu.ContextMenuInfo menuInfo) {
-        // TODO Auto-generated method stub
-        switch (v.getId()) {
-            case R.id.tvColor:
-                menu.add(0, MENU_COLOR_RED, 0, "Red");
-                menu.add(0, MENU_COLOR_GREEN, 0, "Green");
-                menu.add(0, MENU_COLOR_BLUE, 0, "Blue");
-                break;
-            case R.id.tvSize:
-                menu.add(0, MENU_SIZE_22, 0, "22");
-                menu.add(0, MENU_SIZE_26, 0, "26");
-                menu.add(0, MENU_SIZE_30, 0, "30");
-                break;
-        }
-    }
-    public boolean onContextItemSelected(MenuItem item) {
-        // TODO Auto-generated method stub
-        switch (item.getItemId()) {
-            // пункты меню для tvColor
-            case MENU_COLOR_RED:
-                tvColor.setTextColor(Color.RED);
-                tvColor.setText("Text color = red");
-                break;
-            case MENU_COLOR_GREEN:
-                tvColor.setTextColor(Color.GREEN);
-                tvColor.setText("Text color = green");
-                break;
-            case MENU_COLOR_BLUE:
-                tvColor.setTextColor(Color.BLUE);
-                tvColor.setText("Text color = blue");
-                break;
-// пункты меню для tvSize
-            case MENU_SIZE_22:
-                tvSize.setTextSize(22);
-                tvSize.setText("Text size = 22");
-                break;
-            case MENU_SIZE_26:
-                tvSize.setTextSize(26);
-                tvSize.setText("Text size = 26");
-                break;
-            case MENU_SIZE_30:
-                tvSize.setTextSize(30);
-                tvSize.setText("Text size = 30");
-                break;
-        }
-        return super.onContextItemSelected(item);
-    }
-    public void registerForContextMenu(View view) {
-        view.setOnCreateContextMenuListener(this);
+        LinearLayout.LayoutParams lpView = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        TextView tv = new TextView(this);
+        tv.setText("TextView");
+        tv.setLayoutParams(lpView);
+        linLayout.addView(tv);
+
+        Button btn = new Button(this);
+        btn.setText("Button");
+        linLayout.addView(btn, lpView);
+
+
+        LinearLayout.LayoutParams leftMarginParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        leftMarginParams.leftMargin = 50;
+
+        Button btn1 = new Button(this);
+        btn1.setText("Button1");
+        linLayout.addView(btn1, leftMarginParams);
+
+
+        LinearLayout.LayoutParams rightGravityParams = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+        rightGravityParams.gravity = Gravity.RIGHT;
+
+        Button btn2 = new Button(this);
+        btn2.setText("Button2");
+        linLayout.addView(btn2, rightGravityParams);
     }
 }
+
 
 
 
