c7318c4f6ee697253589a143c436558e9e183e69
Jon <djonli@mail.ua>
2018-06-01 21:01:28 +0300

    - lesson 54

--- app/src/main/java/com/example/startandroid/MainActivity.java
+++ app/src/main/java/com/example/startandroid/MainActivity.java
@@ -1,64 +1,48 @@
 package com.example.startandroid;
 
+import java.util.ArrayList;
 import android.app.Activity;
-import android.content.Context;
-import android.database.Cursor;
 import android.os.Bundle;
-import android.widget.ExpandableListView;
-import android.widget.SimpleCursorTreeAdapter;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    ExpandableListView elvMain;
-    DB db;
+    ArrayList<Product> products = new ArrayList<Product>();
+    BoxAdapter boxAdapter;
 
     /** Called when the activity is first created. */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        // подключаемся к БД
-        db = new DB(this);
-        db.open();
+        // создаем адаптер
+        fillData();
+        boxAdapter = new BoxAdapter(this, products);
 
-        // готовим данные по группам для адаптера
-        Cursor cursor = db.getCompanyData();
-        startManagingCursor(cursor);
-        // сопоставление данных и View для групп
-        String[] groupFrom = { DB.COMPANY_COLUMN_NAME };
-        int[] groupTo = { android.R.id.text1 };
-        // сопоставление данных и View для элементов
-        String[] childFrom = { DB.PHONE_COLUMN_NAME };
-        int[] childTo = { android.R.id.text1 };
-
-        // создаем адаптер и настраиваем список
-        SimpleCursorTreeAdapter sctAdapter = new MyAdapter(this, cursor,
-                android.R.layout.simple_expandable_list_item_1, groupFrom,
-                groupTo, android.R.layout.simple_list_item_1, childFrom,
-                childTo);
-        elvMain = (ExpandableListView) findViewById(R.id.elvMain);
-        elvMain.setAdapter(sctAdapter);
+        // настраиваем список
+        ListView lvMain = (ListView) findViewById(R.id.lvMain);
+        lvMain.setAdapter(boxAdapter);
     }
 
-    protected void onDestroy() {
-        super.onDestroy();
-        db.close();
+    // генерируем данные для адаптера
+    void fillData() {
+        for (int i = 1; i <= 20; i++) {
+            products.add(new Product("Product " + i, i * 1000,
+                    R.drawable.ic_launcher, false));
+        }
     }
 
-    class MyAdapter extends SimpleCursorTreeAdapter {
-
-        public MyAdapter(Context context, Cursor cursor, int groupLayout,
-                         String[] groupFrom, int[] groupTo, int childLayout,
-                         String[] childFrom, int[] childTo) {
-            super(context, cursor, groupLayout, groupFrom, groupTo,
-                    childLayout, childFrom, childTo);
+    // выводим информацию о корзине
+    public void showResult(View v) {
+        String result = "Товары в корзине:";
+        for (Product p : boxAdapter.getBox()) {
+            if (p.box)
+                result += "\n" + p.name;
         }
-
-        protected Cursor getChildrenCursor(Cursor groupCursor) {
-            // получаем курсор по элементам для конкретной группы
-            int idColumn = groupCursor.getColumnIndex(DB.COMPANY_COLUMN_ID);
-            return db.getPhoneData(groupCursor.getInt(idColumn));
-        }
+        Toast.makeText(this, result, Toast.LENGTH_LONG).show();
     }
 }
+
 
