c6116c3df37af874172db39d28964dfcb78e8faf
Jon <djonli@mail.ua>
2018-06-01 21:05:29 +0300

    - lesson 56

--- app/src/main/java/com/example/startandroid/MainActivity.java
+++ app/src/main/java/com/example/startandroid/MainActivity.java
@@ -2,26 +2,16 @@ package com.example.startandroid;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    final String LOG_TAG = "myLogs";
-
     String[] data = {"one", "two", "three", "four", "five"};
-    ListView lvMain;
-    ArrayAdapter<String> adapter;
-
-    View header1;
-    View header2;
-
-    View footer1;
-    View footer2;
-
 
     /** Called when the activity is first created. */
     @Override
@@ -29,39 +19,28 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        lvMain = (ListView) findViewById(R.id.lvMain);
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data);
+        // адаптер
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, data);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        // создаем Header и Footer
-        header1 = createHeader("header 1");
-        header2 = createHeader("header 2");
-        footer1 = createFooter("footer 1");
-        footer2 = createFooter("footer 2");
-
-        fillList();
-
-    }
-
-    // формирование списка
-    void fillList() {
-    }
-
-    // нажатие кнопки
-    public void onclick(View v) {
-    }
-
-    // создание Header
-    View createHeader(String text) {
-        View v = getLayoutInflater().inflate(R.layout.header, null);
-        ((TextView)v.findViewById(R.id.tvText)).setText(text);
-        return v;
-    }
-
-    // создание Footer
-    View createFooter(String text) {
-        View v = getLayoutInflater().inflate(R.layout.footer, null);
-        ((TextView)v.findViewById(R.id.tvText)).setText(text);
-        return v;
+        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setAdapter(adapter);
+        // заголовок
+        spinner.setPrompt("Title");
+        // выделяем элемент
+        spinner.setSelection(2);
+        // устанавливаем обработчик нажатия
+        spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                // показываем позиция нажатого элемента
+                Toast.makeText(getBaseContext(), "Position = " + position, Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+            }
+        });
     }
 }
 
