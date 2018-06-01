33d5672889c88925910d2c7de32dbae3fdc0f7a0
Jon <djonli@mail.ua>
2018-06-01 17:32:58 +0300

    - lesson 31

--- app/src/main/java/com/example/startandroid/MainActivity.java
+++ app/src/main/java/com/example/startandroid/MainActivity.java
@@ -1,17 +1,18 @@
 package com.example.startandroid;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends Activity implements OnClickListener {
 
-    TextView tvText;
-    Button btnColor;
-    Button btnAlign;
+    Button btnWeb;
+    Button btnMap;
+    Button btnCall;
 
     /** Called when the activity is first created. */
     @Override
@@ -19,23 +20,37 @@ public class MainActivity extends Activity implements OnClickListener {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        tvText = (TextView) findViewById(R.id.tvText);
+        btnWeb = (Button) findViewById(R.id.btnWeb);
+        btnMap = (Button) findViewById(R.id.btnMap);
+        btnCall = (Button) findViewById(R.id.btnCall);
 
-        btnColor = (Button) findViewById(R.id.btnColor);
-        btnAlign = (Button) findViewById(R.id.btnAlign);
-
-        btnColor.setOnClickListener(this);
-        btnAlign.setOnClickListener(this);
-
+        btnWeb.setOnClickListener(this);
+        btnMap.setOnClickListener(this);
+        btnCall.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        // TODO Auto-generated method stub
-
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.btnWeb:
+                intent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://developer.android.com"));
+                startActivity(intent);
+                break;
+            case R.id.btnMap:
+                intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("geo:55.754283,37.62002"));
+                startActivity(intent);
+                break;
+            case R.id.btnCall:
+                intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:12345"));
+                startActivity(intent);
+                break;
+        }
     }
 }
-
 
 
 
