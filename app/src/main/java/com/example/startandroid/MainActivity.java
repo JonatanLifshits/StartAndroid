e2332d28aacec2c674062ab785647f7e594c5e70
Jon <djonli@mail.ua>
2018-06-03 15:40:23 +0300

    - lesson 77

--- app/src/main/java/com/example/startandroid/MainActivity.java
+++ app/src/main/java/com/example/startandroid/MainActivity.java
@@ -1,46 +1,32 @@
 package com.example.startandroid;
 
 import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TabHost;
 import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends TabActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        TabHost tabHost = findViewById(R.id.tabhost);
-        tabHost.setup();
-        TabHost.TabSpec Tabspec;
+        TabHost tabHost = getTabHost();
+        TabHost.TabSpec tabSpec;
+        tabSpec = tabHost.newTabSpec("tag1");
+        tabSpec.setIndicator("Вкладка 1");
+        tabSpec.setContent(new Intent(this, one.class));
+        tabHost.addTab(tabSpec);
 
-        Tabspec = tabHost.newTabSpec("tag1");
-        Tabspec.setIndicator("вкладка 1");
-        Tabspec.setContent(R.id.tvTab1);
-        TabHost.TabSpec tabSpec = null;
+        tabSpec = tabHost.newTabSpec("tag2");
+        tabSpec.setIndicator("Вкладка 2");
+        tabSpec.setContent(new Intent(this, Main2Activity.class));
         tabHost.addTab(tabSpec);
-        Tabspec = tabHost.newTabSpec("tag2");
-        tabSpec.setIndicator("Вкладка 2", getResources().getDrawable(R.drawable.tab_icon_selector));
-        tabSpec.setContent(R.id.tvTab2);
-        tabHost.addTab(tabSpec);
-        tabSpec = tabHost.newTabSpec("tag3");
-        View v = getLayoutInflater().inflate(R.layout.tab_header,null);
-        tabSpec.setIndicator(v);
-        tabSpec.setContent(R.id.tvTab3);
-        tabHost.addTab(tabSpec);
-        tabHost.setCurrentTabByTag("tag2");
-        tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
-            @Override
-            public void onTabChanged(String tabId) {
-                Toast.makeText(getBaseContext(),"tabId = " + tabId, Toast.LENGTH_SHORT).show();
-            }
-        });
     }
-
 }
-
 
 
