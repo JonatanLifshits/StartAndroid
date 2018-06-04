45e3a0c214b4504f67d5c1320fb9f46ee6629d1f
Jon <djonli@mail.ua>
2018-06-04 16:31:56 +0300

    - lesson 78

--- app/src/main/java/com/example/startandroid/MainActivity.java
+++ app/src/main/java/com/example/startandroid/MainActivity.java
@@ -1,32 +1,48 @@
 package com.example.startandroid;
 
 import android.app.Activity;
-import android.app.TabActivity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TabHost;
-import android.widget.Toast;
+import android.widget.TextView;
 
-public class MainActivity extends TabActivity {
+
+public class MainActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-
-        TabHost tabHost = getTabHost();
-        TabHost.TabSpec tabSpec;
-        tabSpec = tabHost.newTabSpec("tag1");
-        tabSpec.setIndicator("Вкладка 1");
-        tabSpec.setContent(new Intent(this, one.class));
-        tabHost.addTab(tabSpec);
-
-        tabSpec = tabHost.newTabSpec("tag2");
-        tabSpec.setIndicator("Вкладка 2");
-        tabSpec.setContent(new Intent(this, Main2Activity.class));
-        tabHost.addTab(tabSpec);
+        final String TABS_TAG_1 = "Tag1";
+        final String TABS_TAG_2 = "Tag2";
     }
+    TabHost tabHost = getTabHost();
+    TabHost.TabSpec tabSpec;
+
+    tabSpec.setContent(tabFactory);
+        tabSpec.setIndicator("Вкладка 1");
+        tabHost.addTab(tabSpec);
+
+    tabSpec = tabHost.newTabSpec(TABS_TAG_2);
+        tabSpec.setContent(tabFactory);
+        tabSpec.setIndicator("Вкладка 2");
+        tabHost.addTab(tabSpec);
+
 }
 
+    TabHost.TabContentFactory tabFactory = new TabHost.TabContentFactory() {
+
+        @Override
+        public View createTabContent(String tag) {
+            if (tag.equals(TABS_TAG_1)) {
+                return getLayoutInflater().inflate(R.layout.tab, null);
+            } else if (tag.equals(TABS_TAG_2)) {
+                TextView tv = new TextView(MainActivity.this);
+                tv.setText("Это создано вручную");
+                return tv;
+            }
+            return null;
+        }
+    };
+}
 
