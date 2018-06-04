9b42e86912d683d3050a3e1ca62dff2017dd3718
Jon <djonli@mail.ua>
2018-06-04 17:40:51 +0300

    - lesson 86

--- app/src/main/java/com/example/startandroid/MainActivity.java
+++ app/src/main/java/com/example/startandroid/MainActivity.java
@@ -3,53 +3,50 @@ package com.example.startandroid;
 import java.util.concurrent.TimeUnit;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    final String LOG_TAG = "myLogs";
-
+    MyTask mt;
     TextView tvInfo;
 
-    /** Called when the activity is first created. */
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
         tvInfo = (TextView) findViewById(R.id.tvInfo);
-
-        Thread t = new Thread(new Runnable() {
-            public void run() {
-                try {
-                    TimeUnit.SECONDS.sleep(2);
-                    runOnUiThread(runn1);
-                    TimeUnit.SECONDS.sleep(1);
-                    tvInfo.postDelayed(runn3, 2000);
-                    tvInfo.post(runn2);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-        t.start();
     }
 
-    Runnable runn1 = new Runnable() {
-        public void run() {
-            tvInfo.setText("runn1");
-        }
-    };
+    public void onclick(View v) {
+        mt = new MyTask();
+        mt.execute();
+    }
 
-    Runnable runn2 = new Runnable() {
-        public void run() {
-            tvInfo.setText("runn2");
-        }
-    };
+    class MyTask extends AsyncTask<Void, Void, Void> {
 
-    Runnable runn3 = new Runnable() {
-        public void run() {
-            tvInfo.setText("runn3");
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            tvInfo.setText("Begin");
         }
-    };
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            tvInfo.setText("End");
+        }
+    }
 }
