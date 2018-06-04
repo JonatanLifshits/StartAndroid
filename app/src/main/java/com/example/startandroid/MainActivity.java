4ef0982d1fcba31de2a649728a93a382c488f8ac
Jon <djonli@mail.ua>
2018-06-04 16:36:52 +0300

    - lesson 79

--- app/src/main/java/com/example/startandroid/MainActivity.java
+++ app/src/main/java/com/example/startandroid/MainActivity.java
@@ -1,48 +1,73 @@
 package com.example.startandroid;
 
+import java.io.IOException;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.TabHost;
-import android.widget.TextView;
-
+import android.text.TextUtils;
+import android.util.Log;
 
 public class MainActivity extends Activity {
 
+    final String LOG_TAG = "myLogs";
+
+    /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        final String TABS_TAG_1 = "Tag1";
-        final String TABS_TAG_2 = "Tag2";
-    }
-    TabHost tabHost = getTabHost();
-    TabHost.TabSpec tabSpec;
+        String tmp = "";
 
-    tabSpec.setContent(tabFactory);
-        tabSpec.setIndicator("Вкладка 1");
-        tabHost.addTab(tabSpec);
+        try {
+            XmlPullParser xpp = prepareXpp();
 
-    tabSpec = tabHost.newTabSpec(TABS_TAG_2);
-        tabSpec.setContent(tabFactory);
-        tabSpec.setIndicator("Вкладка 2");
-        tabHost.addTab(tabSpec);
+            while (xpp.getEventType() != XmlPullParser.END_DOCUMENT) {
+                switch (xpp.getEventType()) {
+                    // начало документа
+                    case XmlPullParser.START_DOCUMENT:
+                        Log.d(LOG_TAG, "START_DOCUMENT");
+                        break;
+                    // начало тэга
+                    case XmlPullParser.START_TAG:
+                        Log.d(LOG_TAG, "START_TAG: name = " + xpp.getName()
+                                + ", depth = " + xpp.getDepth() + ", attrCount = "
+                                + xpp.getAttributeCount());
+                        tmp = "";
+                        for (int i = 0; i < xpp.getAttributeCount(); i++) {
+                            tmp = tmp + xpp.getAttributeName(i) + " = "
+                                    + xpp.getAttributeValue(i) + ", ";
+                        }
+                        if (!TextUtils.isEmpty(tmp))
+                            Log.d(LOG_TAG, "Attributes: " + tmp);
+                        break;
+                    // конец тэга
+                    case XmlPullParser.END_TAG:
+                        Log.d(LOG_TAG, "END_TAG: name = " + xpp.getName());
+                        break;
+                    // содержимое тэга
+                    case XmlPullParser.TEXT:
+                        Log.d(LOG_TAG, "text = " + xpp.getText());
+                        break;
 
-}
-
-    TabHost.TabContentFactory tabFactory = new TabHost.TabContentFactory() {
-
-        @Override
-        public View createTabContent(String tag) {
-            if (tag.equals(TABS_TAG_1)) {
-                return getLayoutInflater().inflate(R.layout.tab, null);
-            } else if (tag.equals(TABS_TAG_2)) {
-                TextView tv = new TextView(MainActivity.this);
-                tv.setText("Это создано вручную");
-                return tv;
+                    default:
+                        break;
+                }
+                // следующий элемент
+                xpp.next();
             }
-            return null;
-        }
-    };
-}
+            Log.d(LOG_TAG, "END_DOCUMENT");
 
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    XmlPullParser prepareXpp() {
+        return getResources().getXml(R.xml.data);
+    }
+}
