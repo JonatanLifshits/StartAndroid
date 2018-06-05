dc7b15023e48345238b73b3cde29879f490d22b5
Jon <djonli@mail.ua>
2018-06-05 22:59:22 +0300

    - lesson 89

--- app/src/main/java/com/example/startandroid/MainActivity.java
+++ app/src/main/java/com/example/startandroid/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.startandroid;
 
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 
 import android.app.Activity;
 import android.os.AsyncTask;
@@ -10,13 +8,13 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    private MyTask mt;
-    private TextView tvInfo;
-    final String LOG_TAG = "My logs";
+    final String LOG_TAG = "myLogs";
+
+    MyTask mt;
+    TextView tvInfo;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,74 +22,63 @@ public class MainActivity extends Activity {
 
         tvInfo = (TextView) findViewById(R.id.tvInfo);
     }
-    public void OnClick(View v){
-        switch (v.getId()){
-        case R.id.btnStart:
-            mt = new Mytask();
-            mt.execute();
-            break;
-            case R.id.btnGet:
-                showResult();
+
+    public void onclick(View v) {
+        switch (v.getId()) {
+            case R.id.btnStart:
+                mt = new MyTask();
+                mt.execute();
                 break;
-                default:
-                    break;
+            case R.id.btnCancel:
+                cancelTask();
+                break;
+            default:
+                break;
         }
     }
 
-    private void showResult() {
+    private void cancelTask() {
         if (mt == null) return;
-        int result = -1;
-        Log.d(LOG_TAG, "Try to get result");
-        result = mt.get();
-        Log.d(LOG_TAG, "get returns " + result);
-        Toast.makeText(this, "get returns " + result, Toast.LENGTH_LONG).show();
-        class MyTask extends AsyncTask<Void, Void, Integer> {
-            @Override
-            protected void onPreExecute() {
-                super.onPreExecute();
-                tvInfo.setText("Begin");
-                Log.d(LOG_TAG, "Begin");
-            }
+        Log.d(LOG_TAG, "cancel result: " + mt.cancel(true));
+    }
 
+    class MyTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            tvInfo.setText("Begin");
+            Log.d(LOG_TAG, "Begin");
+        }
 
-            @Override
-            protected Integer doInBackground(Void... params) {
-                try {
-                    TimeUnit.SECONDS.sleep(5);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+        protected Void doInBackground(Void... params) {
+            try {
+                for (int i = 0; i < 5; i++) {
+                    TimeUnit.SECONDS.sleep(1);
+//                    if (isCancelled()) return null;
+                    Log.d(LOG_TAG, "isCancelled: " + isCancelled());
                 }
-                return 100500;
+            } catch (InterruptedException e) {
+                Log.d(LOG_TAG, "Interrupted");
+                e.printStackTrace();
             }
-
-            @Override
-            protected void onPostExecute(Integer result) {
-                super.onPostExecute(result);
-                tvInfo.setText("End. Result = " + result);
-                Log.d(LOG_TAG, "End. Result = " + result);
-
-            }
+            return null;
         }
-    }
 
-    private class Mytask extends MyTask {
-    }
-    private void showResult() {
-        if (mt == null) return;
-        int result = -1;
-        try {
-            Log.d(LOG_TAG, "Try to get result");
-            result = mt.get(1, TimeUnit.SECONDS);
-            Log.d(LOG_TAG, "get returns " + result);
-            Toast.makeText(this, "get returns " + result, Toast.LENGTH_LONG).show();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        } catch (TimeoutException e) {
-            Log.d(LOG_TAG, "get timeout, result = " + result);
-            e.printStackTrace();
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            tvInfo.setText("End");
+            Log.d(LOG_TAG, "End");
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+            tvInfo.setText("Cancel");
+            Log.d(LOG_TAG, "Cancel");
+
         }
     }
 }
+
 
