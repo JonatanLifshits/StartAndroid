671735002ad483748c5d10491920356f6883a47b
Jon <djonli@mail.ua>
2018-06-07 11:09:01 +0300

    - lesson 94

--- app/src/main/java/com/example/startandroid/MainActivity.java
+++ app/src/main/java/com/example/startandroid/MainActivity.java
@@ -7,11 +7,14 @@ import android.view.View;
 
 public class MainActivity extends Activity {
 
-    /** Called when the activity is first created. */
+    /**
+     * Called when the activity is first created.
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+}
 
 //    I dont understand!!!
