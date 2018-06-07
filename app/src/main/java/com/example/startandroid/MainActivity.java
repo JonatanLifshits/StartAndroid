8c73e45fef56675a2c12e212d1645f98f60fd60f
Jon <djonli@mail.ua>
2018-06-07 10:53:11 +0300

    - lesson 92

--- app/src/main/java/com/example/startandroid/MainActivity.java
+++ app/src/main/java/com/example/startandroid/MainActivity.java
@@ -3,76 +3,26 @@ package com.example.startandroid;
 import java.util.concurrent.TimeUnit;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
-    MyTask mt;
-    TextView tv;
+    final String LOG_TAG = "MyLogs";
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        Log.d("qwe", "create MainActivity: " + this.hashCode());
 
-        tv = (TextView) findViewById(R.id.tv);
-
-        mt = (MyTask) getLastNonConfigurationInstance();
-        if (mt == null) {
-            mt = new MyTask();
-            mt.execute();
-        }
-        // передаем в MyTask ссылку на текущее MainActivity
-        mt.link(this);
-
-        Log.d("qwe", "create MyTask: " + mt.hashCode());
     }
-
-    public Object onRetainNonConfigurationInstance() {
-        // удаляем из MyTask ссылку на старое MainActivity
-        mt.unLink();
-        return mt;
+    public void onClickStart(View v){
+        startService(new Intent(this, MyService.class));
     }
-
-
-    static class MyTask extends AsyncTask<String, Integer, Void> {
-
-        MainActivity activity;
-
-        // получаем ссылку на MainActivity
-        void link(MainActivity act) {
-            activity = act;
-        }
-
-        // обнуляем ссылку
-        void unLink() {
-            activity = null;
-        }
-
-        @Override
-        protected Void doInBackground(String... params) {
-            try {
-                for (int i = 1; i <= 10; i++) {
-                    TimeUnit.SECONDS.sleep(1);
-                    publishProgress(i);
-                    Log.d("qwe", "i = " + i + ", MyTask: " + this.hashCode()
-                            + ", MainActivity: " + activity.hashCode());
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-            return null;
-        }
-
-        @Override
-        protected void onProgressUpdate(Integer... values) {
-            super.onProgressUpdate(values);
-            activity.tv.setText("i = " + values[0]);
-        }
+    protected void onClickStop(View v) {
+        stopService(new Intent(this, MyService.class));
     }
 }
 
