288939e0db2926f3139d3ffca92e2c5b65a5113e
Jon <djonli@mail.ua>
2018-06-07 10:28:06 +0300

    - lesson 91

--- app/src/main/java/com/example/startandroid/MainActivity.java
+++ app/src/main/java/com/example/startandroid/MainActivity.java
@@ -5,81 +5,73 @@ import java.util.concurrent.TimeUnit;
 import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
     MyTask mt;
-    TextView tvInfo;
+    TextView tv;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        Log.d("qwe", "create MainActivity: " + this.hashCode());
 
-        tvInfo = (TextView) findViewById(R.id.tvInfo);
-    }
+        tv = (TextView) findViewById(R.id.tv);
 
-    public void onclick(View v) {
-        switch (v.getId()) {
-            case R.id.btnStart:
-                startTask();
-                break;
-            case R.id.btnStatus:
-                showStatus();
-                break;
-            default:
-                break;
+        mt = (MyTask) getLastNonConfigurationInstance();
+        if (mt == null) {
+            mt = new MyTask();
+            mt.execute();
         }
+        // передаем в MyTask ссылку на текущее MainActivity
+        mt.link(this);
+
+        Log.d("qwe", "create MyTask: " + mt.hashCode());
     }
 
-    private void startTask() {
-        mt = new MyTask();
-        mt.execute();
-        mt.cancel(false);
-    }
-
-    private void showStatus() {
-        if (mt != null)
-            if (mt.isCancelled())
-            Toast.makeText(this,"CANCELLED,",Toast.LENGTH_SHORT).show();
-        else {
-                Toast.makeText(this, mt.getStatus().toString(), Toast.LENGTH_SHORT).show();
-            }
+    public Object onRetainNonConfigurationInstance() {
+        // удаляем из MyTask ссылку на старое MainActivity
+        mt.unLink();
+        return mt;
     }
 
 
-    class MyTask extends AsyncTask <Void, Void, Void> {
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            tvInfo.setText("Begin");
+    static class MyTask extends AsyncTask<String, Integer, Void> {
+
+        MainActivity activity;
+
+        // получаем ссылку на MainActivity
+        void link(MainActivity act) {
+            activity = act;
+        }
+
+        // обнуляем ссылку
+        void unLink() {
+            activity = null;
         }
 
         @Override
-        protected Void doInBackground(Void... params) {
+        protected Void doInBackground(String... params) {
             try {
-                for (int i = 0; i < 5; i++) {
-                    if (isCancelled()) return null;
+                for (int i = 1; i <= 10; i++) {
                     TimeUnit.SECONDS.sleep(1);
+                    publishProgress(i);
+                    Log.d("qwe", "i = " + i + ", MyTask: " + this.hashCode()
+                            + ", MainActivity: " + activity.hashCode());
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+
             return null;
         }
 
         @Override
-        protected void onPostExecute(Void result) {
-            super.onPostExecute(result);
-            tvInfo.setText("End");
-        }
-
-        @Override
-        protected void onCancelled() {
-            super.onCancelled();
-            tvInfo.setText("Cancel");
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+            activity.tv.setText("i = " + values[0]);
         }
     }
 }
